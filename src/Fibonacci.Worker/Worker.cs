5c0fb12018aec7028a3ce351358abc2171f91931
smolchanovsky <smolchanovsky@gmail.com>
2020-05-09 09:47:01 +0500

    Initial ms templates

---
+++ src/Fibonacci.Worker/Worker.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Fibonacci.Worker
+{
+	public class Worker : BackgroundService
+	{
+		private readonly ILogger<Worker> logger;
+
+		public Worker(ILogger<Worker> logger)
+		{
+			this.logger = logger;
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+				await Task.Delay(1000, stoppingToken);
+			}
+		}
+	}
+}
