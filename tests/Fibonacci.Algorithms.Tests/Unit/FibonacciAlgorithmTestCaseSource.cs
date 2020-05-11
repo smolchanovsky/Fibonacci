b88ba7e80a815dd9fc50365e1e421f33a37e43ff
smolchanovsky <smolchanovsky@gmail.com>
2020-05-11 17:43:16 +0500

    Add tests

---
+++ tests/Fibonacci.Algorithms.Tests/Unit/FibonacciAlgorithmTestCaseSource.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace Fibonacci.Algorithms.Tests.Unit
+{
+	public class FibonacciAlgorithmTestCaseSource
+	{
+		public static IEnumerable ValidCases
+		{
+			get
+			{
+				yield return new TestCaseData((BigInteger)0, (BigInteger)0)
+					.SetDescription("Zero fibonacci number");
+				yield return new TestCaseData((BigInteger)1, (BigInteger)1)
+					.SetDescription("First fibonacci number");
+				yield return new TestCaseData((BigInteger)2, (BigInteger)1)
+					.SetDescription("Second fibonacci number");
+				yield return new TestCaseData((BigInteger)5, (BigInteger)5)
+					.SetDescription("Fifth fibonacci number");
+				yield return new TestCaseData((BigInteger)10, (BigInteger)55)
+					.SetDescription("Tenth fibonacci number");
+				yield return new TestCaseData((BigInteger)33, (BigInteger)3524578)
+					.SetDescription("Thirty-third fibonacci number");
+			}
+		}
+		
+		public static IEnumerable ArgumentExceptionCases
+		{
+			get
+			{
+				yield return new TestCaseData((BigInteger)(-1))
+					.SetDescription("Negative number");
+				yield return new TestCaseData((BigInteger)Int64.MinValue)
+					.SetDescription("Int64.MinValue");
+			}
+		}
+	}
+}
